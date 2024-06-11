f865a23a8e14995572532a02910007517074a247
AleBenitez7 <alejpascua@gmail.com>
2024-06-11 11:08:07 -0400

    se instala tailwind css y se agrega el footer component

---
+++ src/app/components/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    ".components/**/*.{html,ts}",
+    ".pages/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
